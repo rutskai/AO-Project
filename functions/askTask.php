80f927e1717be82beaef95a42099ae12506f4cd0
Ruth <rcolladogarcia@iessonferrer.net>
2025-10-24 17:56:13 +0200

    Update askTask

--- functions/askTask.php
+++ functions/askTask.php
@@ -1,19 +1,26 @@
 <?php
-function askTask(){
+function askTask($tasks){
 
     do{
+        echo "\nIngrese los datos de la tarea:\n";
+        $id=readline("\nId: ");
+        if(!is_numeric($id)){ echo "Error, el ID debe ser numérico.\n"; continue; }
+        $isDuplicated=false;
+        foreach($tasks as $t){
+            if($t->getId()==$id){
+             $isDuplicated=true;
+             break;
+            }
+        }
 
+        if($isDuplicated){ echo "\nId duplicado, por favor, ingrese otro Id."; continue;}
     
-    echo "\nPor favor, ingrese los datos de la tarea:\n";
-    $id=readline("Id: ");
-     if(!is_numeric($id)){ echo "Error, el ID debe ser numérico.\n"; continue; }
-    $title=readline("\nTítulo: ");
-    
-    $description= readline("\nDescripción: "); 
-    if(trim($title)==="" || trim($description)===""){ echo "El texto o/y la descripción no pueden estar vacíos.\n"; continue; }
-    if(is_numeric($title) || is_numeric($description) ){ echo "El título o/y descripción deben ser solo texto. \n"; continue; }
+        $title=readline("\nTítulo: "); 
+        $description= readline("\nDescripción: "); 
+        if(trim($title)=="" || trim($description)==""){ echo "El texto o/y la descripción no pueden estar vacíos.\n"; continue; }
+        if(is_numeric($title) || is_numeric($description) ){ echo "El título o/y descripción deben ser solo texto. \n"; continue; }
 
-    break;
+        break;
 
     }while(true);
 
