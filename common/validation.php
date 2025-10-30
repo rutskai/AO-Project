fb20499e1cd6e2bd5ca4b6ff44293f363169b74d
Ruth <rcolladogarcia@iessonferrer.net>
2025-10-30 19:00:17 +0100

    Update validation.php with comment

--- common/validation.php
+++ common/validation.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * validation
+ *
+ * Valida que el usuario ingrese 's' o 'n'.
+ *
+ * Si el valor no es 's' o 'n', solicita al usuario que ingrese nuevamente hasta que sea válido.
+ *
+ * @param string $value  Valor a validar.
+ * @return string        Valor validado ('s' o 'n').
+ */
+
 function validation($value){
     if($value!="n" && $value != "s"){
         $value = strtolower(readline("\nError, introduce 'n' o 's', por favor: "));
@@ -8,9 +19,5 @@ function validation($value){
 
     return $value;
 }
-
-
-
-
 
 ?>
